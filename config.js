2adba36ee7b7a7a515d28601a37b1a0240176059
Petar Mitchev <petar@imeleon.com>
2014-08-11 05:37:00 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+var api = "http://192.241.169.147:8297";
+
+var coinUnits = 100000000;
+
+var poolHost = "dsh.dashcoin.com";
+
+var irc = "irc.freenode.net/#dashcoin";
+
+var email = "support@democats.org";
+
+var cryptonatorWidget = ["DSH-BTC", "DSH-USD", "DSH-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default-theme.css";
