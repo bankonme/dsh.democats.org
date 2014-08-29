5837ac3aaa35c0f5967c62b9542e97ce41bb1953
Petar Mitchev <petar@imeleon.com>
2014-08-29 18:02:06 +0300

    Test for cryptonator 4

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var irc = "irc.freenode.net/#democats";
 
 var email = "support@democats.org";
 
-var cryptonatorWidget = ["XMR-BTC", "BCN-USD", "{symbol}-EUR"];
+var cryptonatorWidget = ["AEON-BTC", "AEON-USD", "AEON-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
